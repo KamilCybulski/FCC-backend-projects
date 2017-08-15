b9add06b6bc0e9990f2273c3c2ccae51f4816e8d
Kamyl <kamillcybulski@gmail.com>
2017-08-15 23:25:44 +0200

    Implemented file metadata microservice

--- file-metadata/app.js
+++ file-metadata/app.js
@@ -1,14 +1,22 @@
 const express = require('express');
 const config = require('./config/config.js');
+const multer = require('multer');
 
 const app = express();
+const upload = multer({dest: 'uploads/'});
 
 app.use(express.static(__dirname + "/public"));
 
 
 app.get('/', (req, res) => {
-  res.send("elo elo trzy dwa zero");
+  res.sendFile(__dirname + "/public/home.html");
 });
+
+
+app.post('/size', upload.single('file'), (req, res, next) => {
+  res.json({size: req.file.size})
+});
+
 
 
 app.listen(config.PORT, () => {
